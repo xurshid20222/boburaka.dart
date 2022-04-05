6a10497546e17218f00abd5970474820cbbdbd58
Boburbek6010 <boburbekking@gmail.com>
2022-04-05 16:41:20 +0500

    I have Changed

--- bin/boburaka.dart
+++ bin/boburaka.dart
@@ -2,5 +2,6 @@ import 'package:boburaka/boburaka.dart' as boburaka;
 
 void main(List<String> arguments) {
   print('Hello world: ${boburaka.calculate()}!');
+  print('Xurshidjon');
 }
 //aaaaaaaaaaaaaaaaaa
